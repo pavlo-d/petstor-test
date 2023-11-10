d7c1e50c6815c550811ef7455d5ea25046c344a4
Pavlo_D <pavlo.denysyuk@gmail.com>
2023-11-10 14:49:27 +0200

    add User test

--- src/test/java/com/petstore/BaseTest.java
+++ src/test/java/com/petstore/BaseTest.java
@@ -33,5 +33,41 @@ public class BaseTest {
         System.out.println(response.body());
         Assert.assertEquals(response.statusCode(), 200, "User not created");
     }
+    @Test
+    public void loginUser() throws URISyntaxException, IOException, InterruptedException {
+        User user = new User(111,"uName", "lName", "fName","ss@ss.sss",
+                "111","789789",1);
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String body = ow.writeValueAsString(user);
 
+        URI exampleUri = new URI("https://petstore.swagger.io/v2/user/login");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(exampleUri)
+                .GET()
+                .header("Content-Type", "application/json")
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        Assert.assertEquals(response.statusCode(), 200, "User not logged");
+    }
+
+    @Test
+    public void logoutUser() throws URISyntaxException, IOException, InterruptedException {
+        User user = new User(111,"uName", "lName", "fName","ss@ss.sss",
+                "111","789789",1);
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String body = ow.writeValueAsString(user);
+
+        URI exampleUri = new URI("https://petstore.swagger.io/v2/user/logout");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(exampleUri)
+                .GET()
+                .header("Content-Type", "application/json")
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        Assert.assertEquals(response.statusCode(), 200, "User not logged out");
+    }
 }
